94bedd21b114d3ecae2350d0bcf4f9c4576233f0
Sameera Kavinthaka <skavinthaka@gmail.com>
2020-04-07 16:16:43 +0530

    no password

--- dentist/settings.py
+++ dentist/settings.py
@@ -129,8 +129,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587 
 EMAIL_HOST_USER = 'skavinthaka@gmail.com'
-EMAIL_HOST_PASSWORD = 'corei3sameera1990'
-EMIL_USE_SSL == False
+EMAIL_HOST_PASSWORD = ''
+
+# EMIL_USE_SSL == False
 
 django_heroku.settings(locals())
 
