d7a66cf4262d4be99de45cfa631b033dd5362024
Sameera Kavinthaka <skavinthaka@gmail.com>
2020-04-07 15:59:39 +0530

    password chaged back to skavinthaka@gmail.com

--- website/views.py
+++ website/views.py
@@ -17,7 +17,7 @@ def contact(request):
 			message_name,	 # subject
 			message,		 # messages
 			message_email,	 # from email
-			['sakuni_kanchana@yahoo.com'] #to email
+			['skavinthaka@gmail.com'] #to email
 			)
 
 
@@ -55,7 +55,7 @@ def appointment(request):
 			'Appointment Request',	 # subject
 			'message',		 # messages
 			'message_email',	 # from email
-			['sakuni_kanchana@yahoo.com'] #to email
+			['skavinthaka@gmail.com'] #to email
 			)
 
 
